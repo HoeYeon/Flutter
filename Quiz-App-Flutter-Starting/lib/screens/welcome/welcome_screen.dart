0b67aec784c7d149779a4c91949e739a59761676
Hoeyeon <shy9546@naver.com>
2022-06-16 01:07:53 +0900

    [QAF] welcome screen

--- Quiz-App-Flutter-Starting/lib/screens/welcome/welcome_screen.dart
+++ Quiz-App-Flutter-Starting/lib/screens/welcome/welcome_screen.dart
@@ -1,8 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Stack(
+      children: [
+        WebsafeSvg.asset("assets/icons/bg.svg"),
+        SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Spacer(),
+            Text("Let's Play Quiz.",
+                style: Theme.of(context).textTheme.headline4.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Text("Enter your information below"),
+            Spacer(),
+            TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF1C2341),
+                  hintText: "Full Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  )),
+            ),
+            Spacer(),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                  decoration: BoxDecoration(
+                    gradient: kPrimaryGradient,
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
+                  child: Text("Let's Start Quiz",
+                      style: Theme.of(context)
+                          .textTheme
+                          .button
+                          .copyWith(color: Colors.black))),
+            ),
+            Spacer(flex: 2),
+          ]),
+        ))
+      ],
+    ));
   }
 }
