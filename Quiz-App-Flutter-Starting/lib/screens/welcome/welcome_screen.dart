765d501d132bdc656e1d5a7dd15be3c5947d5c5e
HoeYeon Son <hoeyeonson@HoeYeonui-MacBookPro.local>
2022-12-10 18:03:28 +0900

    impl welcome screen

--- Quiz-App-Flutter-Starting/lib/screens/welcome/welcome_screen.dart
+++ Quiz-App-Flutter-Starting/lib/screens/welcome/welcome_screen.dart
@@ -6,50 +6,55 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        WebsafeSvg.asset("assets/icons/bg.svg"),
-        SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Spacer(),
-            Text("Let's Play Quiz.",
+        body: Stack(children: [
+      WebsafeSvg.asset("assets/icon/bg.svg"),
+      SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Spacer(flex: 2), // 2/6
+            Text("Lets Play Quiz,",
                 style: Theme.of(context).textTheme.headline4.copyWith(
                     color: Colors.white, fontWeight: FontWeight.bold)),
-            Text("Enter your information below"),
-            Spacer(),
+            Text("Enter your informations below"),
+            Spacer(), // 1/6
             TextField(
               decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color(0xFF1C2341),
-                  hintText: "Full Name",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                  )),
+                filled: true,
+                fillColor: Color(0xFF1C2341),
+                hintText: "Full Name",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                )),
+              ),
             ),
-            Spacer(),
+            Spacer(), // 1/6
             InkWell(
               onTap: () {},
               child: Container(
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(kDefaultPadding * 0.75),
-                  decoration: BoxDecoration(
-                    gradient: kPrimaryGradient,
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                  ),
-                  child: Text("Let's Start Quiz",
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: Colors.black))),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                decoration: BoxDecoration(
+                  gradient: kPrimaryGradient,
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Text(
+                  "Lets Start Quiz",
+                  style: Theme.of(context)
+                      .textTheme
+                      .button
+                      .copyWith(color: Colors.black),
+                ),
+              ),
             ),
-            Spacer(flex: 2),
-          ]),
-        ))
-      ],
-    ));
+            Spacer(flex: 2), // 전체 정의된 flex(6개 기본이 1) 2개 만큼의 비중을 가져감 즉, 2/6
+          ],
+        ),
+      ))
+    ]));
   }
 }
