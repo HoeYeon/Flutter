b1c2128b9b732da3be50034bf63a15ea7c47a294
HoeYeon <shy9546@naver.com>
2023-02-20 18:45:24 +0900

    [WIP] product list

--- Exercise/starter/lib/main.dart
+++ Exercise/starter/lib/main.dart
@@ -1,60 +1,76 @@
 import 'package:flutter/material.dart';
+import 'product.dart';
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({Key? key, required this.products}) : super(key: key);
+
+  final List<Product> products;
+
+  // The framework calls createState the first time a widget appears at a given
+  // location in the tree. If the parent rebuilds and uses the same type of
+  // widget (with the same key), the framework re-uses the State object
+  // instead of creating a new State object.
+
+  @override
+  ShoppingListState createState() => ShoppingListState();
+}
+
+class ShoppingListState extends State<ShoppingList> {
+  final Set<Product> _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      // When a user changes what's in the cart, you need to change
+      // _shoppingCart inside a setState call to trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          print(product.name);
+          print(product.hashCode);
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
 
 void main() {
-  runApp(MaterialApp(
-    title: "My app",
-    home: MyScaffold(),
-  ));
-}
+  print("hashCode in Main-------");
+  print(Product(name: 'Eggs').hashCode);
+  print(Product(name: 'Eggs').hashCode);
+  print("-----------------------");
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({required this.title});
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 56.0,
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        decoration: BoxDecoration(color: Colors.blue[500]),
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null,
-            ),
-            Expanded(
-              child: title,
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
-        ));
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-        child: Column(
-      children: [
-        MyAppBar(
-          title: Text("this is title"),
-        ),
-        Expanded(
-          child: Center(
-            child: Text("Hello World"),
-          ),
-        )
+  runApp(const MaterialApp(
+    title: 'Shopping App',
+    home: ShoppingList(
+      products: <Product>[
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Eggs'),
+        Product(name: 'Meat'),
+        Product(name: 'Chocolate chips'),
       ],
-    ));
-  }
+    ),
+  ));
 }
