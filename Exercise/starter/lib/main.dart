606f726e3c9d982d3f599ce72e7c34c5d94e42de
HoeYeon <shy9546@naver.com>
2023-04-04 23:22:00 +0900

    remove unused file

--- Exercise/starter/lib/main.dart
+++ Exercise/starter/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'product.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
